c717b288ff750f2f2b9063f8358dcc5ebda9fca1
its-kayes <kayes.ek8@gmail.com>
2023-09-10 14:21:55 +0600

    User Login Done !

--- src/modules/smart-village/registration/reg.controller.ts
+++ src/modules/smart-village/registration/reg.controller.ts
@@ -5,13 +5,15 @@ import { ISmartVillageReg } from "./reg.interface";
 import { SmartVillageService } from "./reg.service";
 import AppError from "../../../errors/AppError";
 import bcrypt from "bcryptjs";
+import { Request, Response } from "express";
+import { SmartVillageReg } from "./reg.model";
 
 export const hashPassword = async (password: string) => {
   const salt = await bcrypt.genSalt(10);
   return await bcrypt.hash(password, salt);
 };
 
-const addUser = catchAsync(async (req, res) => {
+const addUser = catchAsync(async (req: Request, res: Response) => {
   const { name, email, password, role } = req.body as ISmartVillageReg;
 
   if (!name || !email || !password || !role) {
@@ -35,6 +37,26 @@ const addUser = catchAsync(async (req, res) => {
   throwResponse(res, result, httpStatus.OK, "User added successfully", true);
 });
 
+const login = catchAsync(async (req: Request, res: Response) => {
+  const { email, password } = req.body as ISmartVillageReg;
+
+  const isUser = await SmartVillageReg.findOne({ email });
+  if (!isUser) {
+    throw new AppError("User not found", httpStatus.NOT_FOUND);
+  }
+
+  const isPasswordMatch = await bcrypt.compare(
+    password as string,
+    isUser.password as string
+  );
+
+  if (!isPasswordMatch)
+    throw new AppError("Password not match", httpStatus.BAD_REQUEST);
+
+  throwResponse(res, isUser, httpStatus.OK, "User Login Successfully", true);
+});
+
 export const SmartVillageController = {
   addUser,
+  login,
 };
--- src/modules/smart-village/registration/reg.routes.ts
+++ src/modules/smart-village/registration/reg.routes.ts
@@ -4,5 +4,6 @@ import { SmartVillageController } from "./reg.controller";
 const router: Router = Router();
 
 router.post("/create", SmartVillageController.addUser);
+router.post("/login", SmartVillageController.login);
 
 export { router as SmartVillageRegRoutes };
