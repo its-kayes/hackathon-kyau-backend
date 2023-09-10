cd47c74a73af27095fccdc3ab30dd6325fa026da
its-kayes <kayes.ek8@gmail.com>
2023-09-10 13:54:29 +0600

    hash Password

--- src/modules/smart-village/registration/reg.controller.ts
+++ src/modules/smart-village/registration/reg.controller.ts
@@ -4,9 +4,22 @@ import catchAsync from "../../../util/catchAsync";
 import { ISmartVillageReg } from "./reg.interface";
 import { SmartVillageService } from "./reg.service";
 import AppError from "../../../errors/AppError";
+import bcrypt from "bcryptjs";
+
+export const hashPassword = async (password: string) => {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+};
 
 const addUser = catchAsync(async (req, res) => {
   const { name, email, password, role } = req.body as ISmartVillageReg;
+
+  if (!name || !email || !password || !role) {
+    throw new AppError(
+      "Please provide all the details",
+      httpStatus.BAD_REQUEST
+    );
+  }
 
   const result = await SmartVillageService.addUser({
     name,
--- src/modules/smart-village/registration/reg.service.ts
+++ src/modules/smart-village/registration/reg.service.ts
@@ -1,7 +1,12 @@
+import { hashPassword } from "./reg.controller";
 import { ISmartVillageReg } from "./reg.interface";
 import { SmartVillageReg } from "./reg.model";
 
 const addUser = async (data: ISmartVillageReg) => {
+  const hashPass = await hashPassword(data.password as string);
+
+  data.password = hashPass;
+
   const result = await SmartVillageReg.create(data);
   return result;
 };
